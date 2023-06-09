ac74df80914ada8ca68c561b644abd90bc2011ba
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-06-09 15:27:50 +0200

    Adding tests

--- test.sh
+++ test.sh
@@ -8,14 +8,14 @@ for f in "$@"; do
     ext="${f#*.}"
     echo -n $f
     if [[ "${ext}" == 'n3' ]]; then
-        eye --nope --quiet --pass $lib $f | grep ':is true' > /dev/null 2>&1 
+        eye --nope --quiet --pass $lib $f lib-rdfs.n3 | grep ':is true' > /dev/null 2>&1 
         if [ $? -eq 0 ]; then
             echo " OK"
         else
             echo " FAILED"
         fi
     elif [[ "${ext}" == 'n3s' ]]; then
-        eye --nope --quiet --blogic $lib $f | grep ':is true' > /dev/null 2>&1 
+        eye --nope --quiet --blogic $lib $f lib-rdfs.n3s | grep ':is true' > /dev/null 2>&1 
         if [ $? -eq 0 ]; then
             echo " OK"
         else
